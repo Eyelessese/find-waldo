372a392793e3e2f1e3aa995f16a30cba5bacbfe7
Ryan Ewanicke <reiz.talsean@gmail.com>
2018-08-30 10:19:31 -0600

    fixed forEach loop

--- find-waldo.js
+++ find-waldo.js
@@ -1,14 +1,17 @@
 // The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+function findWaldo(arr, found)
+{
+  arr.forEach(function(element) 
+  { 
+    if (element === "Waldo")
+    {
+      found(arr, element);   // execute callback
     }
-  }
+  });
 }
 
-function actionWhenFound(index) {
-  console.log("Found Waldo at index " + index + "!");
+function actionWhenFound(item, thing) {
+  console.log("Found Waldo at index " + item.indexOf(thing) + "!");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
